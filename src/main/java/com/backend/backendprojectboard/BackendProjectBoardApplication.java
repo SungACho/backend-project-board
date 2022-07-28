c938fe3d034d3385dd79aec21272096b30cc3c0c
sungaCho <sunga0720@naver.com>
2022-07-29 03:11:08 +0900

    #7 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술 : 
    
    * 스프링 웹 
    * 엑츄레이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/backend/backendprojectboard/BackendProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.backend.backendprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProjectBoardApplication.class, args);
+    }
+
+}
